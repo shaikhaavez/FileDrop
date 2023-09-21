64ca80ae146f585bbf0b27a188cd5f9b51afb659
shaikhaavez <116972980+shaikhaavez@users.noreply.github.com>
2023-09-22 00:03:56 +0530

    Update index.js

--- public/index.js
+++ public/index.js
@@ -2,7 +2,7 @@ const dropZone = document.querySelector(".dropzone");
 const browseBtn = document.querySelector(".browseBtn");
 const fileInput = document.querySelector("#fileInput");
 
-const host = "https://filedrop-go02.onrender.com/";
+const host = " ";
 const uploadurl = `${host}/api/files`;
 //const uploadurl = `${host/api/files`;
 
